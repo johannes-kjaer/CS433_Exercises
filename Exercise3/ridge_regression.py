3c774c2ea9a00b8edaafca0141bd7174bddfb425
Johannes Kjaer <77483678+johannes-kjaer@users.noreply.github.com>
2023-10-12 20:31:28 +0200

    Began testing out k-fold crossvalidation

---
+++ Exercise3/ridge_regression.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+ 
+    w = np.linalg.solve(len(y) * lambda_ * np.identity(tx.shape[1]) + tx.T@tx , tx.T@y)  # Computing w by way of the normal equations
+    return w
